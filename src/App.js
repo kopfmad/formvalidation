9cfcaaee59ce12f8421101b3eb5fc8f67fc6c30e
andresantana <santana.agr@gmail.com>
2021-08-03 16:27:06 -0300

    it works using alerts

--- src/App.js
+++ src/App.js
@@ -1,7 +1,104 @@
-import React from "react";
+import React, {useState} from "react";
 
 function App() {
-  return <div className="App"></div>;
+
+let qtyErrors = 0
+
+// HOOKS
+let [name,setName] = useState('')
+let [phone,setPhone] = useState('')
+let [password,setPassword] = useState('')
+let [cpassword,setCpassword] = useState('')
+
+// FUNCTIONS
+// CHANGING INPUTFIELDS
+function changeName(e){
+setName(e.target.value)
+}
+
+function changePhone(e){
+setPhone(e.target.value)
+}
+
+function changePassword(e){
+setPassword(e.target.value)
+}
+
+function changeCpassword(e){
+setCpassword(e.target.value)
+}
+
+// ANALYZING INPUTFIELD
+
+function checkName (){
+  if (name.trim().length <5 | name === "" | name === null){
+    alert("Please insert your name")
+    qtyErrors+=1
+
+  }
+}
+
+function checkPhone (){
+  if (phone.trim().length <8 | phone === "" | phone === null){
+    alert("Please insert your phone number")
+    qtyErrors+=1
+  }
+}
+
+function checkPassword (){
+  if (password.trim().length <8 | password === "" | password === null){
+    alert("Password must have at least 8 characters")
+    qtyErrors+=1
+  }
+}
+
+function checkCpassword (){
+  if (cpassword !== password | cpassword === "" | cpassword === null){
+    alert("Password confirmation incorrect")
+    qtyErrors+=1
+  }
+}
+
+// CLIKING THE BUTTON
+function clicked (e) {
+  qtyErrors=0
+checkName()
+checkPhone()
+checkPassword()
+checkCpassword()
+
+if(qtyErrors>0){e.preventDefault()}
+}
+
+
+// APP
+
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Form Validation</h1>
+        <form>
+          <div>
+            {" "}
+            <input type="text" placeholder="Name" onChange={changeName} value={name}/>{" "}
+          </div>
+          <div>
+            <input type="email" placeholder="E-mail" />
+          </div>
+          <div>
+            <input type="number" placeholder="Phone" onChange={changePhone} value={phone}/>
+          </div>
+          <div>
+            <input type="password" placeholder="Password" onChange={changePassword} value={password}/>
+          </div>
+          <div>
+            <input type="password" placeholder="Confirm Password" onChange={changeCpassword} value={cpassword}/>
+          </div>
+          <button onClick={clicked}>Submit</button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default App;
